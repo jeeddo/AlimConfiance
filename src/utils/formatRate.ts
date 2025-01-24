755b709b90f44836248f8e2d0929e58425ce366b
Pierro <pierrematar1912@gmail.com>
2025-01-24 11:51:19 +0100

    refactor : formatRate utility enhanced

--- src/utils/formatRate.ts
+++ src/utils/formatRate.ts
@@ -2,15 +2,15 @@ import  Ratings ,{type Rating } from "../types/ratings.d";
 
  export default function formatRate (rating: string): Rating {
     switch (rating) {
-      case 'Très satisfaisant':
+      case Ratings.Excellent:
         return {rate: Ratings.Excellent, color: 'excellent'};
-      case 'Satisfaisant':
+      case Ratings.Good:
         return {rate: Ratings.Good, color: 'good'};
-      case 'A améliorer':
+      case Ratings.Average:
         return {rate: Ratings.Average, color: 'average'};
       case 'A corriger de manière urgente':
         return {rate: Ratings.Poor,color: 'poor'};
       default:
         throw new Error('Invalid rating');
     }
-  };
+  }
