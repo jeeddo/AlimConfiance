aa0186173fef9c80535460aeb40d58b508e4bcdc
Pierro <pierrematar1912@gmail.com>
2025-02-07 21:43:43 +0100

    adjustments

--- src/components/main/AutocompleteInput.tsx
+++ src/components/main/AutocompleteInput.tsx
@@ -7,6 +7,7 @@ import type { AutocompleteLiProps } from "./AutocompleteLi"
 import useClickOutside from "../../hooks/useClickOutside"
 import { getLocations } from "../../services/location"
 import { getSpecificRestaurant } from "../../services/restaurant"
+import { MAX_MOBILE_DEVICES_WIDTH } from "../../utils/constants"
 export interface AutocompleteInputProps extends Omit<AutocompleteLiProps, 'value' | 'setLiClicked'> {
     inputValue: string,
     isInForm: boolean,
@@ -26,17 +27,21 @@ export default function AutocompleteInput({inputValue, isSearchBtnClicked, isInF
     const [input, setInput] = useState('')
     const [autocompleteValues, setAutocompleteValues] = useState<AutocompleteValue[]>([])
     const [showInput, setShowInput] = useState(false)
+    const inputRef = useRef<HTMLInputElement | null>(null)
 
    
      
     const clickOutsideAction = () => {
+        if (window.innerWidth <= MAX_MOBILE_DEVICES_WIDTH && input) {
+            setInput('')
+        }
         setAutocompleteVisibility('hidden')
         setIsAutocompleteVisible?.(false)
         setShowInput(false)
     }
 
     useClickOutside(divElement, clickOutsideAction, false, autocompleteVisibility === '')
-         
+
          
      const handleOnFocusAutocompleteVisibility = () => {
         if (inputValue || input)  {
@@ -75,13 +80,22 @@ export default function AutocompleteInput({inputValue, isSearchBtnClicked, isInF
     const handleClass = (): string => {
         let letClass = className
         if (!isInForm && showInput) letClass += ' w-[220px]'
-        else if (!isInForm && !showInput ) letClass += ' w-10 placeholder:invisible'
+        else if (!isInForm && !showInput ) letClass += ' w-12 placeholder:invisible'
         else return 'w-full rounded'
         return letClass ?? ''
     }
-
-    return <div onClick={() => { if (window.innerWidth <= 640) setShowInput(true)}} ref={divElement} className={`${isInForm ? 'w-full' : ''} relative`}>
-    <input value={inputValue} onFocus={handleOnFocusAutocompleteVisibility} onChange={(e) => handleInputValueChange?.(e) ?? setInput(e.target.value)} className={`${handleClass()} px-4 py-1 shadow-md bg-slate-100 focus:ring-2 focus:shadow-lg transition-all duration-500 outline-none`} type="text" autoComplete='off' placeholder={isSearchBtnClicked || !isInForm ? 'Search a restaurant' : 'Enter a localisation'} id='localisation' />
+    const onClick = () => {
+         if (window.innerWidth <= MAX_MOBILE_DEVICES_WIDTH && !showInput && inputRef.current) {
+            const end = inputRef.current.value.length;
+            setShowInput(true)
+            inputRef.current?.focus()
+            inputRef.current?.setSelectionRange(end, end)
+ 
+        } 
+    }
+    
+    return <div onClick={onClick} ref={divElement} className={`${isInForm ? 'w-full' : ''} relative`}>
+    <input value={inputValue ?? input} ref={inputRef} onFocus={handleOnFocusAutocompleteVisibility} onChange={(e) => handleInputValueChange?.(e) ?? setInput(e.target.value)} className={`${handleClass()} px-4 py-1 shadow-md bg-slate-100 focus:ring-2 focus:shadow-lg transition-all duration-500 outline-none`} type="text" autoComplete='off' placeholder={isSearchBtnClicked || !isInForm ? 'Search a restaurant' : 'Enter a localisation'} id='localisation' />
     <FontAwesomeIcon className={`absolute top-1/2 -translate-y-1/2 ${!showInput && !isInForm ? 'left-1/2 -translate-x-1/2 sm:translate-x-0 sm:left-auto sm:right-3' : 'right-3 '}`} icon={isSearchBtnClicked || !isInForm ? searchIcon : locationIcon} />
     <ul  className={autocompleteVisibility + ` absolute top-[125%] left-1/2 -translate-x-1/2 flex flex-col justify-center items-start gap-2 ${isInForm ? 'bg-primary' : 'bg-secondary'} rounded-xl py-3 px-4 ${!isSearchBtnClicked && isInForm ? 'w-11/12' : 'w-full'} max-h-[200px] overflow-y-auto overflow-x-hidden z-50`}>
     {autocompleteValues.length > 0 && autocompleteValues.map((value: AutocompleteValue, i) => (<AutocompleteLi key={i} setLiClicked={setLiClicked} {...props} value={value} />))}
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
 export const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL || 'http://localhost:8000';
 export const LOCATION_PATH = import.meta.env.VITE_BACKEND_LOCATION_PATH || '/api/location';
 export const RESTAURANT_PATH = import.meta.env.VITE_BACKEND_RESTAURANT_PATH || '/api/restaurant';
+export const MAX_MOBILE_DEVICES_WIDTH = 640;
