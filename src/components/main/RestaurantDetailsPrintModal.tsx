116464ffd3a1801e76ce309b74bb3b2da0d56cf7
Pierro <pierrematar1912@gmail.com>
2025-02-11 11:06:37 +0100

    refactor : print modal

--- src/components/main/RestaurantDetailsPrintModal.tsx
+++ src/components/main/RestaurantDetailsPrintModal.tsx
@@ -12,10 +12,11 @@ interface CardDetailsPrintModalProps {
     setPrintRestaurantDetails: (restaurant: React.SetStateAction<Restaurant | null>) => void,
     restaurantDetails: Restaurant | null,
 }
+
+type PrintModalContentProps = Pick<CardDetailsPrintModalProps, 'restaurantDetails'>
+type PrintModalHeaderProps = Pick<CardDetailsPrintModalProps, 'setPrintRestaurantDetails'>
 export default function RestaurantDetailsPrintModal({setPrintRestaurantDetails, restaurantDetails}: CardDetailsPrintModalProps) {
-    const [isPrintBtnClicked, setPrintBtnClicked] = useState(false)
     const [isModalOpen, setIsModalOpen] = useState(false)
-    const contentRef = useRef<HTMLDivElement | null>(null);
     const printModal = useRef<HTMLDivElement | null>(null);
 
     useEffect(() => {
@@ -25,6 +26,25 @@ export default function RestaurantDetailsPrintModal({setPrintRestaurantDetails, 
 
 
     useClickOutside(printModal, () => setPrintRestaurantDetails(null), false, isModalOpen)
+   
+
+    return  isModalOpen && restaurantDetails !== null && (<div ref={printModal} className='z-50 flex flex-col justify-center items-start gap-2 w-full xs:w-3/4 bg-bg absolute top-0 left-0 rounded-md xs:text-sm text-xs '>
+    <PrintModalHeader setPrintRestaurantDetails={setPrintRestaurantDetails} />
+    <PrintModalContent restaurantDetails={restaurantDetails} />
+    
+</div>)
+}
+
+function PrintModalHeader({setPrintRestaurantDetails}: PrintModalHeaderProps) {
+    return <header className='h-10 bg-indigo flex justify-center items-center w-full rounded-t-md'>
+    <button onClick={() => setPrintRestaurantDetails(null)} className='sm:text-base text-sm hover:opacity-65 transition duration-300 w-full h-full'> <FontAwesomeIcon icon={closeIcon} />
+    </button>
+</header>
+}
+function PrintModalContent({restaurantDetails}: PrintModalContentProps) {
+    const [isPrintBtnClicked, setPrintBtnClicked] = useState(false)
+    const contentRef = useRef<HTMLDivElement | null>(null);
+
     const reactToPrint = useReactToPrint({
         contentRef,
         onAfterPrint: () => setPrintBtnClicked(false)
@@ -33,40 +53,35 @@ export default function RestaurantDetailsPrintModal({setPrintRestaurantDetails, 
     if (isPrintBtnClicked) reactToPrint()
    }, [isPrintBtnClicked])
 
-    return  isModalOpen && restaurantDetails !== null && (<div ref={printModal} className='z-50 flex flex-col justify-center items-start gap-2 w-full xs:w-3/4 bg-bg absolute top-0 left-0 rounded-md xs:text-sm text-xs '>
-    <header className='h-10 bg-indigo flex justify-center items-center w-full rounded-t-md'>
-        <button onClick={() => setPrintRestaurantDetails(null)} className='sm:text-base text-sm hover:opacity-65 transition duration-300 w-full h-full'> <FontAwesomeIcon icon={closeIcon} />
-        </button>
-    </header>
-    <main ref={contentRef} className='flex flex-col justify-center items-start md:gap-5 gap-4 p-5 border-dashed border-2 border-indigo m-4 rounded'>
-        <div  className={`flex ${isPrintBtnClicked ? 'justify-between' : 'justify-around'} items-center gap-2 mb-1`}>
-            <img className='w-1/5 shadow' src={Logo2} alt="Logo ministère de l'agriculture et de l'alimentation" />
-            <h2>alimconfiance.gouv.fr</h2>
-            { !isPrintBtnClicked &&
-                <button onClick={() => setPrintBtnClicked(true)} className='bg-main flex justify-center items-center gap-2 text-white px-3 py-1 rounded active:scale-95 transition duration-500 hover:bg-indigo hover:text-main hover:shadow-indigo shadow-lg'>Imprimer<FontAwesomeIcon icon={printerIcon} /></button>
-            }
+   if (restaurantDetails)
+    return <main ref={contentRef} className='flex flex-col justify-center items-start md:gap-5 gap-4 p-5 border-dashed border-2 border-indigo m-4 rounded'>
+    <div  className={`flex ${isPrintBtnClicked ? 'justify-between' : 'justify-around'} items-center gap-2 mb-1`}>
+        <img className='w-1/5 shadow' src={Logo2} alt="Logo ministère de l'agriculture et de l'alimentation" />
+        <h2>alimconfiance.gouv.fr</h2>
+        { !isPrintBtnClicked &&
+            <button onClick={() => setPrintBtnClicked(true)} className='bg-main flex justify-center items-center gap-2 text-white px-3 py-1 rounded active:scale-95 transition duration-500 hover:bg-indigo hover:text-main hover:shadow-indigo shadow-lg'>Imprimer<FontAwesomeIcon icon={printerIcon} /></button>
+        }
+    </div>
+    <div className='flex flex-col justify-center items-start gap-4'>
+        <div className='flex flex-col justify-center items-start gap-3'>
+            <h3 className='xs:text-base text-sm text-main font-semibold'>{restaurantDetails.name}</h3>
+            <p className='bg-main px-3 py-1 text-white rounded-full shadow-md'>{restaurantDetails.activity}</p>
         </div>
-        <div className='flex flex-col justify-center items-start gap-4'>
-            <div className='flex flex-col justify-center items-start gap-3'>
-                <h3 className='xs:text-base text-sm text-main font-semibold'>{restaurantDetails.name}</h3>
-                <p className='bg-main px-3 py-1 text-white rounded-full shadow-md'>{restaurantDetails.activity}</p>
-            </div>
-            <div className='flex flex-col justify-center items-start gap-1'>
-                <p>{restaurantDetails.address}</p>
-                <p>{restaurantDetails.postalCode + ' ' + restaurantDetails.city}</p>
-            </div>
-        </div>
-        <hr className='text-slate-500 w-1/3 rounded-full' />
         <div className='flex flex-col justify-center items-start gap-1'>
-            <p><strong>Date de l'inspection : </strong>{restaurantDetails.inspectionDate}</p>
-            <p className='italic underline'>Validité 1 an</p>
+            <p>{restaurantDetails.address}</p>
+            <p>{restaurantDetails.postalCode + ' ' + restaurantDetails.city}</p>
         </div>
-        <div className='relative max-w-full mx-auto mt-5'>
-            <p className='text-base sm:text-xl xs:px-12 xs:py-5 px-8 py-3 border border-indigo hover:border-2 hover:scale-105 hover:shadow-md xs:hover:shadow-lg transition-all duration-700 rounded-lg shadow'>{(restaurantDetails.rating as Rating).rate}</p>
-            <div className={clsx(`h-2 w-2 rounded-full absolute top-2 right-2`, {'bg-poor': (restaurantDetails.rating as Rating).color === 'poor', 'bg-average': (restaurantDetails.rating as Rating).color === 'average', 'bg-good': (restaurantDetails.rating as Rating).color === 'good', 'bg-excellent': (restaurantDetails.rating as Rating).color === 'excellent'})}>
+    </div>
+    <hr className='text-slate-500 w-1/3 rounded-full' />
+    <div className='flex flex-col justify-center items-start gap-1'>
+        <p><strong>Date de l'inspection : </strong>{restaurantDetails.inspectionDate}</p>
+        <p className='italic underline'>Validité 1 an</p>
+    </div>
+    <div className='relative max-w-full mx-auto mt-5'>
+        <p className='text-base sm:text-xl xs:px-12 xs:py-5 px-8 py-3 border border-indigo hover:border-2 hover:scale-105 hover:shadow-md xs:hover:shadow-lg transition-all duration-700 rounded-lg shadow'>{(restaurantDetails.rating as Rating).rate}</p>
+        <div className={clsx(`h-2 w-2 rounded-full absolute top-2 right-2`, {'bg-poor': (restaurantDetails.rating as Rating).color === 'poor', 'bg-average': (restaurantDetails.rating as Rating).color === 'average', 'bg-good': (restaurantDetails.rating as Rating).color === 'good', 'bg-excellent': (restaurantDetails.rating as Rating).color === 'excellent'})}>
 
-            </div>
         </div>
-    </main>
-</div>)
+    </div>
+</main>
 }
